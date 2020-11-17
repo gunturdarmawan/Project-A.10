b3d901299c7c2f0cf5584170c9d7c3e8b3ed71fc
gunturdarmawan <19523166@student.uii.ac.id>
2020-11-18 01:04:30 +0700

    membat animasi dengan gsap

--- js/style.js
+++ js/style.js
@@ -1,11 +1,26 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
+      gsap.from('.content-left', {
+        stagger:1,
+        opacity:0, 
+        duration:2,
+        y: -20, delay: 0.5
+        });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+      var tl = gsap.timeline({defaults:{duration: .7, ease: Back.easeOut.config(2), opacity:0}});
+
+      tl.from("#img-content", {delay: 2, opacity:0, scale: .3, transformOrigin: 'left'}, "=.2");
+      gsap.registerPlugin(MotionPathPlugin);
+
+      gsap.from("#phone", {
+        duration: 5, 
+        repeat: -1,
+        delay: 1,
+        yoyo: true,
+        ease: "power1.inOut",
+        motionPath:{
+          path: "#Pathmain",
+          align: "#Pathmain",
+          autoRotate: false,
+          alignOrigin: [0.5, 0.5]
+        }
+      });
